d83ca5a40774d9a7a2d1fc07955795add496ebe1
mbeatrice1 <mahorobeatrice9@gmail.com>
2021-03-13 18:20:20 +0200

    added user class

--- user.py
+++ user.py
@@ -1,4 +1,16 @@
 class User:
+    """
+    Class that generates new instances of contacts.
+    """
+
+ user_list=[]   
+
+def save_user(self)
+ 
+
+User.user_list.append(self)
+
+
 def __init__(self,username,password):
 self.username=username
 self.password=password
